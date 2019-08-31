021b7ee49e8c15ebe2ea30bdccbabb8a24ddd1d5
Simranjit <simranjitkaur94mec@gmail.com>
2019-08-31 11:16:20 -0400

    Added MVC

---
+++ BookMyCar/Controllers/CustomersController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookMyCar.Models;
+
+namespace BookMyCar.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();//disposeable object
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers/Index
+        public ActionResult Index()
+        {
+
+            //var customers = _context.Customers;//deferred execution
+            //return Content("Hellp Customers");
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        //GET : Customers/Details/Id
+        public ActionResult Details(int? Id)
+        {
+            //var customers = _context.Customers;
+            var customers = GetCustomers();
+            foreach (var customer in customers)
+            {
+                if (customer.Id == Id)
+                {
+                    return View(customer);
+                }
+            }
+            return Content("Customer not found");
+
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            var Customers = new List<Customer>
+            {
+               new Customer{Name =  "John Smith",Id = 1000},
+                new Customer{Name =  "Mary Williams",Id=1001}
+
+            };
+            return Customers;
+        }
+
+    }
+}
